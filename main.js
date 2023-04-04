9e9a6f76ee1a23baf09499da2adb5b8ace959d54
Faizan  Alam <faizan.alam@sourcefuse.com>
2023-04-04 16:52:53 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,60 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const request = require('request')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+app.use(cors({ credentials: true }))
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json())
+
+const { OAUTH_CONSUMER_KEY, OAUTH_TOKEN, OAUTH_NONCE, OAUTH_SIGNATURE } = process.env
+
+app.post('/send', bodyParser.json(), async (req, res) => {
+
+  const recipientID = "1603309278"
+  const text = "Hi This is a test message"
+
+    // URL Link for twitter endpoint
+    const urlLink = 'https://api.twitter.com/1.1/direct_messages/events/new.json';
+    
+    // Generating timestamp
+    const ts = Math.floor(new Date().getTime()/1000);
+   const timestamp = ts;
+
+    // Authorization Parameters
+    const params = {
+        "oauth_version"          : "1.0",
+        "oauth_consumer_key"     : OAUTH_CONSUMER_KEY,
+        "oauth_token"            : OAUTH_TOKEN,
+        "oauth_timestamp"        :  1680603872,
+        "oauth_nonce"            : OAUTH_NONCE,
+        "oauth_signature_method" : "HMAC-SHA1",
+        "oauth_signature"        : OAUTH_SIGNATURE
+    };
+    //HMAC-SHA1
+    const dataString = `{"event": {"type": "message_create", "message_create": {"target": { "recipient_id": "${recipientID}"},"message_data": {"text": "${text}"}}}}`;
+
+    const options = {
+        url: urlLink,
+        headers: {
+         "Authorization": `OAuth oauth_consumer_key="${params.oauth_consumer_key}", oauth_nonce= ${params.oauth_nonce}, oauth_signature= ${params.oauth_signature}, oauth_signature_method="HMAC-SHA1", oauth_timestamp=${params.oauth_timestamp},oauth_token="${params.oauth_token}", oauth_version=${params.oauth_version}`,
+         "Content-type": 'application/json'
+        },
+        body: dataString
+      }
+
+    request.post(options, (error, response, body) =>{
+        console.log(response.body);
+        return res.send(response.body)
+    });
+})
+
+app.post('/receive', async(req,res) => {
+  console.log("Incoming webhook=> ",res.body)
+  res.send("received")
+})
+
+app.listen(3000,()=>
+    console.log("Listening on port: 3000")
+)
